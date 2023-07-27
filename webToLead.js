492b056531c048f2fbf0bb1cefa7be9610144ea2
bernadetteramirez16 <100861946+bernadetteramirez16@users.noreply.github.com>
2023-07-27 16:07:29 +0800

    Add files via upload

--- webToLead.js
+++ webToLead.js
@@ -1,1 +1,9 @@
+
+// JavaScript code to get the current URL
 var currentURL = window.location.href;
+console.log('currentURL=>', currentURL);
+
+// Function to assign the currentURL to the input field
+function assignCurrentURL() {
+    document.getElementById('urlInput').value = currentURL;
+}
